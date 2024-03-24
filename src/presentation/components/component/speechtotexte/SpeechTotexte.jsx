2bb32dba2bfa7fc914681d948a27d6fd95df3873
LucasNickBryan <rlucasnickbryan@gmail.com>
2024-03-24 09:42:13 +0300

    Speech to text

--- src/presentation/components/component/speechtotexte/SpeechTotexte.jsx
+++ src/presentation/components/component/speechtotexte/SpeechTotexte.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import VoiceVisualizer from "./voiceVisualizer";
 import "./speechTotexte.scss";
+import axios from "axios";
+import { IP_API } from "presentation/utils/config";
 
 const SpeechTotexte = () => {
   const voiceVisualizer = new VoiceVisualizer();
   const [result, setResult] = useState("");
   const [recording, isrecording] = useState(false);
   let SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition,
+    window.SpeechRecognition || window.webkitSpeechRecognition,
     recognition;
 
   const recordbtn = () => {
@@ -31,7 +33,7 @@ const SpeechTotexte = () => {
       recognition.start();
       voiceVisualizer.startVisualization();
 
-      function test () {
+      function test() {
         console.log("Mety")
       }
 
@@ -44,8 +46,7 @@ const SpeechTotexte = () => {
           if (speechResult.toLowerCase().includes("salut")) {
             isrecording(false);
             stopRecording();
-            console.log("test")
-            speak("salut comment comment comment comment ça vas");
+            speak("salut comment ça vas");
           } else if (speechResult.toLowerCase().includes("météo")) {
             isrecording(false);
             stopRecording();
@@ -63,14 +64,37 @@ const SpeechTotexte = () => {
           speak(
             "vous voulez voir la carte ok je vais vous rediriger vers la carte"
           );
-        } else {
+
+        } else if (speechResult.toLowerCase().includes("porte") && speechResult.toLowerCase().includes("ouvrir")) {
+          console.log("Tafiditra")
+          isrecording(false);
+          stopRecording();
+          axios.get(IP_API + "item/status/65ff5141213a60d5c342255e").then((res) => {
+            speak(
+              "Votre porte a été ouvert avec succès"
+            );
+          }).catch(err => {
+            console.log(err)
+            speak(
+              "echec d'ouverture de la porte"
+            );
+          })
+        }
+        else if (speechResult.toLowerCase().includes("télécommande")) {
+          isrecording(false);
+          stopRecording();
+          speak("La télécommande se trouve sur la table de nuit.");
+        }
+
+
+        else {
           setResult(" " + result + " " + speechResult);
         }
       };
       recognition.onspeechend = () => {
         speechToText();
       };
-      
+
 
       recognition.onerror = (event) => {
         isrecording(false);
@@ -117,68 +141,68 @@ const SpeechTotexte = () => {
   }
   return (
     <div className="vocal-assistance">
-    <div
-      className="speech-bloc"
-    >
-      <h1>Assistante Vocale</h1>
-      <div id="visualizer-container">
-        <canvas id="output" width="250" height="200"></canvas>
-      </div>
-      <button onClick={recordbtn} className="btn-voice">
-        <div className={`container-svg-btn ${recording ? "active" : ""}`}>
-          <svg
-            width="800px"
-            height="800px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V11C17 13.7614 14.7614 16 12 16C9.23858 16 7 13.7614 7 11V8Z"
-              stroke="#1C274C"
-              stroke-width="1.5"
-            />
-            <path
-              opacity="0.5"
-              d="M13.5 8L17 8"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-            <path
-              opacity="0.5"
-              d="M13.5 11L17 11"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-            <path
-              opacity="0.5"
-              d="M7 8L9 8"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-            <path
-              opacity="0.5"
-              d="M7 11L9 11"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-            <path
-              opacity="0.5"
-              d="M20 10V11C20 15.4183 16.4183 19 12 19M4 10V11C4 15.4183 7.58172 19 12 19M12 19V22"
-              stroke="#1C274C"
-              stroke-width="1.5"
-              stroke-linecap="round"
-            />
-          </svg>
+      <div
+        className="speech-bloc"
+      >
+        <h1>Assistante Vocale</h1>
+        <div id="visualizer-container">
+          <canvas id="output" width="250" height="200"></canvas>
         </div>
-        {recording ? "Arreter" : "Parler"}
-      </button>
-      <p className="rqt">Votre requête : {result}</p>
-    </div>
+        <button onClick={recordbtn} className="btn-voice">
+          <div className={`container-svg-btn ${recording ? "active" : ""}`}>
+            <svg
+              width="800px"
+              height="800px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V11C17 13.7614 14.7614 16 12 16C9.23858 16 7 13.7614 7 11V8Z"
+                stroke="#1C274C"
+                stroke-width="1.5"
+              />
+              <path
+                opacity="0.5"
+                d="M13.5 8L17 8"
+                stroke="#1C274C"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              />
+              <path
+                opacity="0.5"
+                d="M13.5 11L17 11"
+                stroke="#1C274C"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              />
+              <path
+                opacity="0.5"
+                d="M7 8L9 8"
+                stroke="#1C274C"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              />
+              <path
+                opacity="0.5"
+                d="M7 11L9 11"
+                stroke="#1C274C"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              />
+              <path
+                opacity="0.5"
+                d="M20 10V11C20 15.4183 16.4183 19 12 19M4 10V11C4 15.4183 7.58172 19 12 19M12 19V22"
+                stroke="#1C274C"
+                stroke-width="1.5"
+                stroke-linecap="round"
+              />
+            </svg>
+          </div>
+          {recording ? "Arreter" : "Parler"}
+        </button>
+        <p className="rqt">Votre requête : {result}</p>
+      </div>
     </div>
   );
 };
